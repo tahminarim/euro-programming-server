edfa607a795d54df2e1a4e7a9709f68becebd881
tahminarim <tahminarim9@gmail.com>
2022-10-25 03:36:00 +0200

    data of 7 courses added with image and details

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express= require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
+const coursedetails = require('./data/coursedetails.json');
 
 app.get('/', (req,res)=>{
     res.send('server running');
 })
+
+
+app.get('/courses', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    }
+    else {
+        const course = news.filter(n => n.course_id === id);
+        res.send(course);
+    }
+})
+
+app.get('/blogs', (req, res) =>{
+    res.send('blogs');
+});
+
+// app.get('/news/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectedNews = news.find(n => n._id === id);
+//     res.send(selectedNews);
+// });
 
 app.listen(port, ()=>{
     console.log('euro programming running',port);
